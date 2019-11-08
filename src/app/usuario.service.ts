0a4d926ff9fbf84a584cf4a5e55fa687b859fe8f
marcorodas <marco.rodas@pucp.edu.pe>
2019-11-08 16:43:12 -0500

    2 files modified

--- src/app/usuario.service.ts
+++ src/app/usuario.service.ts
@@ -2,7 +2,7 @@ export class UsuarioService {
   loggedUserMail: string;
   userList = [
     { correo: "marco.rodas@gmail.com", contrasena: "admin" },
-    { correo: "xtian.amir@gmail.com", contrasena: "elgato" },
+    { correo: "xtian.amir@gmail.com", contrasena: "thecat" },
     { correo: "marco.ardp.ims@gmail.com", contrasena: "12345" },
     { correo: "marco.rodas@as-supportpe.com", contrasena: "area51" },
     { correo: "marco.rodas@pucp.pe", contrasena: "skynet" }
